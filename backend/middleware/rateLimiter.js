df815eaca0d837bea5ead4ec62952d3caedf33be
AjeetkumarYadav07 <iamsupertrader07@gmail.com>
2026-03-06 16:03:14 +0530

    add changes

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,20 +1,13 @@
 const rateLimit = require("express-rate-limit");
+const { ipKeyGenerator } = require("express-rate-limit");
 
-//  const convertLimiter = createLimiter(40);
-// const freeLimiter = createLimiter(20);
-
-const createLimiter = (maxRequests = 10) => {
+const createLimiter = (limit) => {
   return rateLimit({
-    windowMs: 5 * 60 * 1000, // 15 minutes 
-    max: maxRequests,
-
-    keyGenerator: (req) => {
-      if (req.user) {
-        return `user_${req.user.id}`;
-      }
-      return `ip_${req.ip}`;
-    },
-
+    windowMs: 60 * 1000, // 1 minute
+    max: limit,
+    standardHeaders: true,
+    legacyHeaders: false,
+    keyGenerator: (req) => ipKeyGenerator(req.ip),
     message: {
       success: false,
       message: "Daily limit reached",
@@ -22,4 +15,4 @@ const createLimiter = (maxRequests = 10) => {
   });
 };
 
-module.exports = createLimiter;
+module.exports = { createLimiter };
