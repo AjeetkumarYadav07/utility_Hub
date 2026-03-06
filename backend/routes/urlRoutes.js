0087ed6dedd04a9a6b462eb5a647d456679cc0ef
AjeetkumarYadav07 <iamsupertrader07@gmail.com>
2026-03-06 17:25:24 +0530

    update rate limit

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const createLimiter = require("../middleware/rateLimiter");
+const { createLimiter } = require("../middleware/rateLimiter");
 
 const {
   createUrlController,
